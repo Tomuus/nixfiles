97f454df500268562d452d7cf96f0966b2c9c666
Tomus <tomasz1.mrozinski@proton.me>
2025-08-07 19:20:04 +0200

    Added home-manager
    Just added home manager and set up plain home.nix file

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager?ref=release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ...} @ inputs:
+  outputs = { self, nixpkgs, home-manager, ...} @ inputs:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
@@ -16,6 +18,11 @@
       modules = [
         ./hosts/Latitude7310/configuration.nix
 	./modules/de/hyprland.nix
+	home-manager.nixosModules.home-manager {
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.tomus = import ./hosts/Latitude7310/home.nix;
+	}
       ];
     };
   };
