c7d43a0e3052886bb1c26752c354e48a643dd3e5
Tomus <tomasz1.mrozinski@proton.me>
2025-08-11 21:00:13 +0200

    Nvf installation
    Still learning it but maybe some configs comming soon

--- flake.nix
+++ flake.nix
@@ -5,20 +5,31 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
     home-manager.url = "github:nix-community/home-manager?ref=release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ...} @ inputs:
+  outputs = { self, nixpkgs, home-manager, nvf, ...} @ inputs:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in
   {
+    pankages.${system}.default = 
+      (nvf.lib.neovimConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+	modules = [./home/nix-managed/nvf.nix];
+      }).neovim;
     nixosConfigurations.Latitude7310 = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/Latitude7310/configuration.nix
 	./modules/de/hyprland.nix
 	./modules/dev/cpp.nix
+        nvf.nixosModules.default
 	home-manager.nixosModules.home-manager {
 	  home-manager.useGlobalPkgs = true;
 	  home-manager.useUserPackages = true;
