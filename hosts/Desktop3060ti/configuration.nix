183d5796a7a6570b70fe0d553565162724bbdcf3
Tomus <tomasz1.mrozinski@proton.me>
2025-08-28 17:08:16 +0200

    New packages + updated hyprland config
    Damn i need to make the monitors config indepent from home manager
    Nah maybe later

--- hosts/Desktop3060ti/configuration.nix
+++ hosts/Desktop3060ti/configuration.nix
@@ -5,12 +5,14 @@
 {
   imports = [
     ./hardware-configuration.nix
+    ./packages.nix
     ./../common
     #./packages.nix
     "${mods}/de/hyprland.nix"
     "${mods}/de/plasma6.nix"
     #"${mods}/dev/cpp.nix"
     "${mods}/gpu/nvidia.nix"
+    "${mods}/dm/sddm.nix"
   ];
   networking.hostName = "Desktop3060ti";
 }
