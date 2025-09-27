da5691d95f3e49b73ff2f9dd36ac7d1304a2a4aa
Tomus <tomasz1.mrozinski@proton.me>
2025-09-27 18:07:06 +0200

    New packages for Latitude 7310

--- hosts/Latitude7310/packages.nix
+++ hosts/Latitude7310/packages.nix
@@ -1,12 +1,23 @@
 { pkgs, lib, ... }:
 
 {
+  services.udisks2.enable = true;
+  security.polkit.enable = true;
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
              "twingate"
   ];
   nixpkgs.config.allow.unfree = true;
   environment.systemPackages = with pkgs; [
     twingate
+    kdePackages.dolphin
+    unzip
+    vlc
+    flac
+    sshfs
+    samba
+    psmisc
+    cifs-utils
+    libreoffice-qt6-fresh
   ];
   services.power-profiles-daemon.enable = true;
   #services.twingate.enable = true;
