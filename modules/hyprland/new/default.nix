7f33a9f2036915fb1ddb7ea8a6111c4a41a8d9fd
Tomuus <tomasz1.mrozinski@proton.me>
2025-12-19 17:31:45 +0100

    Added blueman + pakages to hyprland

--- modules/hyprland/new/default.nix
+++ modules/hyprland/new/default.nix
@@ -25,7 +25,22 @@
 		oh-my-posh
 		hyprmon
 		lxqt.lxqt-policykit
+		xdg-desktop-portal
+		xdg-desktop-portal-gtk
+		xdg-desktop-portal-hyprland
+		kdePackages.gwenview
+		kdePackages.plasma-integration
+		shared-mime-info
+		xdg-utils
+		desktop-file-utils
 	];
+
+	environment.etc."xdg/menus/applications.menu".source = "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+
+	xdg.mime.enable = true;
+	xdg.menus.enable = true;
+	xdg.portal.enable = true;
+
 
 	home-manager.users.${userName} = import ./config;
 }
