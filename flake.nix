3ac6b17755610907532766d02630819e61df11cb
Tomuus <tomasz1.mrozinski@proton.me>
2026-01-14 15:38:06 +0100

    Removed unused configurations
    I don't even own these laptops anymore

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
-  description = "Nixos config flake";
+  description = "Let's waste time debbuging config instead of code";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+
     home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     
@@ -14,7 +15,7 @@
     
   };
 
-  outputs = { self, nixpkgs, home-manager, nvf, nixpkgs-unstable, caelestiaShell, ... } @ inputs:
+  outputs = { self, nixpkgs, home-manager, nixpkgs-unstable, caelestiaShell, ... } @ inputs:
   let
     unstbl = nixpkgs-unstable;
     mods = ./modules; # Just so i can import modules everywhere i want without big paths 
@@ -23,47 +24,23 @@
   in
   {
     nixosConfigurations = {
-      Latitude7310 = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-        specialArgs = { inherit inputs mods userName fullName; };
-        modules = [
-          ./hosts/Latitude7310/configuration.nix
-          nvf.nixosModules.default
-          home-manager.nixosModules.home-manager { home-manager.users.${userName} = import ./home { inherit userName fullName; }; } #If it works DON'T TOUCH IT
-        ];
-      };
-
-      Desktop3060ti = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
+			Desktop3060ti = nixpkgs.lib.nixosSystem {
+    		system = "x86_64-linux";
         specialArgs = { inherit inputs mods userName fullName; };
         modules = [
           ./hosts/Desktop3060ti/configuration.nix
-          nvf.nixosModules.default
-          home-manager.nixosModules.home-manager { home-manager.users.${userName} = import ./home { inherit userName fullName; }; }
-        ];
+          home-manager.nixosModules.home-manager 
+					];
       };
-
-      Latitude5410 = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-        specialArgs = { inherit inputs mods  userName fullName; };
-        modules = [
-          ./hosts/Latitude5410/configuration.nix
-          inputs.spicetify-nix.nixosModules.default
-          nvf.nixosModules.default
-          home-manager.nixosModules.home-manager { home-manager.users.${userName} = import ./home { inherit userName fullName; }; }
-        ];
-      };
-
-      Makbuk = nixpkgs.lib.nixosSystem {
-    system = "aarch64-linux";
+      
+			Makbuk = nixpkgs.lib.nixosSystem {
+    		system = "aarch64-linux";
         specialArgs = { inherit unstbl inputs mods userName fullName; };
         modules = [
           ./hosts/Makbuk/configuration.nix
-          #nvf.nixosModules.default
-          home-manager.nixosModules.home-manager #If it works DON'T TOUCH IT
-        ];
+          home-manager.nixosModules.home-manager 
+					];
       };
-
     };
   };
 }
