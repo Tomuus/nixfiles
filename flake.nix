ede0edf3cdbfa109ba46f2d3a11f72fc53ae5a71
Tomuus <tomasz1.mrozinski@proton.me>
2026-01-28 19:02:50 +0100

    Introduction of flake parts to the config
    Vimjoyer is too influential for my config
    I just watched his latest video and I'll move to dendritic pattern

--- flake.nix
+++ flake.nix
@@ -1,65 +1,69 @@
 {
-  description = "Let's waste time debbuging config instead of code";
+	description = "Let's waste time debbuging config instead of code";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+		nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
 
-    home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
-    caelestiaShell = {
-      url = "github:caelestia-dots/shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    
-  };
+		home-manager.url = "github:nix-community/home-manager?ref=release-25.11";
+		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, home-manager, nixpkgs-unstable, caelestiaShell, ... } @ inputs:
-  let
-    unstbl = nixpkgs-unstable;
-    mods = ./modules; # Just so i can import modules everywhere i want without big paths 
-    userName = "tomus";
-    fullName = "Tomus";
-  in
-  {
-    nixosConfigurations = {
-			Desktop3060ti = nixpkgs.lib.nixosSystem {
-    		system = "x86_64-linux";
-        specialArgs = { inherit inputs mods userName fullName; };
-        modules = [
-          ./hosts/Desktop3060ti/configuration.nix
-					./common
-          home-manager.nixosModules.home-manager 
-					];
-      };
-      
-			Makbuk = nixpkgs.lib.nixosSystem {
-    		system = "aarch64-linux";
-        specialArgs = { inherit unstbl inputs mods userName fullName; };
-        modules = [
-          ./hosts/Makbuk/configuration.nix
-					./common
-          home-manager.nixosModules.home-manager 
-					];
-      };
-    };
-  };
+		caelestiaShell = {
+			url = "github:caelestia-dots/shell";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+		flake-parts.url = "github:hercules-ci/flake-parts";
+	};
+
+	outputs = { self, nixpkgs, home-manager, nixpkgs-unstable, flake-parts, ... } @ inputs:
+	let
+		unstbl = nixpkgs-unstable;
+		mods = ./modules; # Just so i can import modules everywhere i want without big paths 
+		userName = "tomus";
+		fullName = "Tomus";
+	in
+		flake-parts.lib.mkFlake { inherit inputs; } {
+			flake = {
+				nixosConfigurations = {
+					Desktop3060ti = nixpkgs.lib.nixosSystem {
+						system = "x86_64-linux";
+						specialArgs = { inherit inputs mods userName fullName; };
+						modules = [
+							./hosts/Desktop3060ti/configuration.nix
+								./common
+								home-manager.nixosModules.home-manager 
+						];
+					};
+
+					Makbuk = nixpkgs.lib.nixosSystem {
+						system = "aarch64-linux";
+						specialArgs = { inherit unstbl inputs mods userName fullName; };
+						modules = [
+							./hosts/Makbuk/configuration.nix
+								./common
+								home-manager.nixosModules.home-manager 
+						];
+					};
+				};
+			};
+			systems = [ "x86_64-linux" "aarch64-linux" ];
+		};
 }
 
 /*
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠄
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢤⡄⠂⢺⠃⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⡪⠊⠙⢷⣤⡆⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⡪⠊⠀⠀⠀⢈⠝⠁⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⣾⣯⣦⡀⠀⢀⠔⠁⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⡠⡮⠋⠀⠙⢿⣷⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⣀⣠⡾⠊⠀⠀⢀⢔⠕⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⢷⣸⣊⠀⠀⠀⢰⠕⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⡀⣀⠜⠓⣝⠵⣂⣔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠨⠓⣅⠔⡄⠉⠙⠂⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-⠀⠑⠠⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠄
+	 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠔⠁⠀
+	 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⢤⡄⠂⢺⠃⠀⠀⠀
+	 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⡪⠊⠙⢷⣤⡆⠀⠀⠀⠀
+	 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⡪⠊⠀⠀⠀⢈⠝⠁⠀⠀⠀⠀
+	 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⣾⣯⣦⡀⠀⢀⠔⠁⠀⠀⠀⠀⠀⠀
+	 ⠀⠀⠀⠀⠀⠀⠀⡠⡮⠋⠀⠙⢿⣷⠔⠁⠀⠀⠀⠀⠀⠀⠀⠀
+	 ⠀⠀⠀⠀⣀⣠⡾⠊⠀⠀⢀⢔⠕⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	 ⠀⠀⠀⢷⣸⣊⠀⠀⠀⢰⠕⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	 ⠀⡀⣀⠜⠓⣝⠵⣂⣔⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	 ⠨⠓⣅⠔⡄⠉⠙⠂⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+	 ⠀⠑⠠⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
-Properly vaccinated by Tomuus
-*/
+	 Properly vaccinated by Tomuus
+ */
